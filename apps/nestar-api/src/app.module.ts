61bbffbfa00830ca7c2a38a511576b0a20a8f86c
Anvarjon <anvarjondevelop@gmail.com>
2026-04-04 19:38:04 +0900

    feat: configure env variables

--- apps/nestar-api/src/app.module.ts
+++ apps/nestar-api/src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [ConfigModule.forRoot()], // nestar-api server da .env ni o'qishga imkon yaratadi
   controllers: [AppController],
   providers: [AppService],
 })
