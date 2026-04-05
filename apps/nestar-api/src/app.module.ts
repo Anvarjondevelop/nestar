c5e6f526faadd63536878696f08a666d1b7de721
Anvarjon <anvarjondevelop@gmail.com>
2026-04-05 16:12:12 +0900

    feat: integration GraphQLModule and build first GraphQL api

--- apps/nestar-api/src/app.module.ts
+++ apps/nestar-api/src/app.module.ts
@@ -2,10 +2,22 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriver } from '@nestjs/apollo';
+import { AppResolver } from './app.resolver';
 
 @Module({
-  imports: [ConfigModule.forRoot()], // nestar-api server da .env ni o'qishga imkon yaratadi
-  controllers: [AppController],
-  providers: [AppService],
+	//shu class ichida loyiha qismlarini ro‘yxatdan o‘tkazaman
+	imports: [
+		ConfigModule.forRoot(), // nestar-api server da .env ni o'qishga imkon yaratadi //forRoot()=>Modulni boshlang‘ich sozlama bilan ishga tushir
+		GraphQLModule.forRoot({
+			driver: ApolloDriver,
+			playground: true,
+			uploads: false,
+			autoSchemaFile: true,
+		}),
+	],
+	controllers: [AppController],
+	providers: [AppService, AppResolver],
 })
 export class AppModule {}
