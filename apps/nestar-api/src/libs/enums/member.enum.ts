70e452bce7dfeefc80a853419e983e2f227df4cb
Anvarjon <anvarjondevelop@gmail.com>
2026-04-05 18:58:47 +0900

    feat: create Schema Models and project ennums

---
+++ apps/nestar-api/src/libs/enums/member.enum.ts
@@ -0,0 +1,30 @@
+//to'g'ridan-to'g'ni bu enumlarni graphQL bilan ishlataolmaymiz
+//ishlatish uchun enumlarni ro'yhatga oladigan package kerak bo'ladi
+import { registerEnumType } from '@nestjs/graphql';
+
+export enum MemberType {
+	USER = 'USER',
+	AGENT = ' AGENT',
+	ADMIN = 'ADMIN',
+}
+registerEnumType(MemberType, {
+	name: 'MemberType',
+});
+
+export enum MemberStatus {
+	ACTIVE = 'ACTIVE',
+	BLOCK = ' BLOCK',
+	DELETE = 'DELETE',
+}
+registerEnumType(MemberStatus, {
+	name: 'MemberStatus',
+});
+
+export enum MemberAuthType {
+	PHONE = 'PHONE',
+	EMAIL = 'EMAIL',
+	TELEGRAM = 'TELEGRAM',
+}
+registerEnumType(MemberAuthType, {
+	name: 'MemberAuthType',
+});
