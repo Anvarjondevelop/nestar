80dbd8eabb3ea0e262c9a73287319636f6819738
Anvarjon <anvarjondevelop@gmail.com>
2026-04-05 18:00:16 +0900

    feat: build project standarts

--- apps/nestar-api/src/app.module.ts
+++ apps/nestar-api/src/app.module.ts
@@ -5,17 +5,22 @@ import { ConfigModule } from '@nestjs/config';
 import { GraphQLModule } from '@nestjs/graphql';
 import { ApolloDriver } from '@nestjs/apollo';
 import { AppResolver } from './app.resolver';
+import { ComponentsModule } from './components/components.module';
+import { DatabaseModule } from './database/database.module';
 
 @Module({
 	//shu class ichida loyiha qismlarini ro‘yxatdan o‘tkazaman
 	imports: [
 		ConfigModule.forRoot(), // nestar-api server da .env ni o'qishga imkon yaratadi //forRoot()=>Modulni boshlang‘ich sozlama bilan ishga tushir
 		GraphQLModule.forRoot({
-			driver: ApolloDriver,
-			playground: true,
+			//Bu GraphQL modulini NestJS ichiga ulayapti.
+			driver: ApolloDriver, //Bu yerda GraphQL qaysi engine/driver bilan ishlashini aytyapti.
+			playground: true, //test qiladigan maxsus sahifa.
 			uploads: false,
-			autoSchemaFile: true,
+			autoSchemaFile: true, //“Schema faylni NestJS o‘zi avtomatik generatsiya qilsin”
 		}),
+		ComponentsModule, //HTTP
+		DatabaseModule, //TCP
 	],
 	controllers: [AppController],
 	providers: [AppService, AppResolver],
