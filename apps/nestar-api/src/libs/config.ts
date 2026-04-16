fdb78a4506047763764ebfd310ad046ab4c38bc3
Anvarjon <anvarjondevelop@gmail.com>
2026-04-16 17:27:24 +0900

    feat: develop single and multi image uploader mutation GraphQL API's

--- apps/nestar-api/src/libs/config.ts
+++ apps/nestar-api/src/libs/config.ts
@@ -3,6 +3,33 @@ import { ObjectId } from 'bson';
 export const availableAgentSorts = ['createdAt', 'updatedAt', 'memberLikes', 'memberView', 'memberRank'];
 export const availableMemberSorts = ['createdAt', 'updatedAt', 'memberLikes', 'memberView'];
 
+/* IMAGE CONFIGURATION */
+const { v4: uuidv4 } = require('uuid');
+import * as path from 'path';
+
+export const validMimeTypes = ['image/png', 'image/jpg', 'image/jpeg'];
+export const getSerialForImage = (filename: string) => {
+	const ext = path.parse(filename).ext;
+	return uuidv4() + ext;
+};
+
 export const shapeIntoMongoObjectId = (target: any) => {
 	return typeof target === 'string' ? new ObjectId(target) : target;
 };
+
+/* POSTMAN
+  imageUploader
+  operations: { "query": "mutation ImageUploader($file: Upload!, $target: String!) { imageUploader(file: $file, target: $target) }", "variables": { "file": null, "target": "member" }}
+  map: { "0": ["variables.file"] }
+  0: File
+ 
+  imagesUploader
+  operations: { "query": "mutation ImagesUploader($files: [Upload!]!, $target: String!) { imagesUploader(files: $files, target: $target) }", "variables": { "files": [null, null, null, null], "target": "property" }}
+  map: { "0": ["variables.files.0"], "1": ["variables.files.1"], "2": ["variables.files.2"], "3": ["variables.files.3"], "4": ["variables.files.4"] }
+  0: File
+  1: File
+  2: File
+  3: File
+  4: File
+ 
+  **/
