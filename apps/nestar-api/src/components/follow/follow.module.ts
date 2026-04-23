44173417741589a8db5237dc39c35f6cccafccb4
Anvarjon <anvarjondevelop@gmail.com>
2026-04-23 12:26:00 +0900

    feat: create follow related resolver and service model

--- apps/nestar-api/src/components/follow/follow.module.ts
+++ apps/nestar-api/src/components/follow/follow.module.ts
@@ -1,8 +1,16 @@
 import { Module } from '@nestjs/common';
 import { FollowService } from './follow.service';
 import { FollowResolver } from './follow.resolver';
+import { Mongoose } from 'mongoose';
+import { MongooseModule } from '@nestjs/mongoose';
+import FollowSchema from '../../schemas/Follow.model';
+import { AuthModule } from '../auth/auth.module';
+import { Member } from '../../libs/dto/member/member';
+import { MemberModule } from '../member/member.module';
 
 @Module({
-  providers: [FollowResolver, FollowService],
+	imports: [MongooseModule.forFeature([{ name: 'Follow', schema: FollowSchema }]), AuthModule, MemberModule],
+
+	providers: [FollowResolver, FollowService],
 })
 export class FollowModule {}
--- apps/nestar-api/src/components/follow/follow.service.ts
+++ apps/nestar-api/src/components/follow/follow.service.ts
@@ -1,4 +1,15 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { AuthService } from '../auth/auth.service';
+import { Follower, Following } from '../../libs/dto/follow/follow';
+import { MemberService } from '../member/member.service';
 
 @Injectable()
-export class FollowService {}
+export class FollowService {
+	constructor(
+		@InjectModel('Follow') private readonly memberModel: Model<Follower | Following>,
+		private authService: AuthService, //inctance olinyapdi
+		private memberService: MemberService,
+	) {}
+}
