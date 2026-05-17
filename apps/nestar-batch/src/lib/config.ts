e03ea0dfc2794f2fc84513a7f0c1e50d1ed96262
Anvarjon <anvarjondevelop@gmail.com>
2026-05-17 15:30:25 +0900

    fix: modify getProperties GraphQL API logic

--- apps/nestar-batch/src/lib/config.ts
+++ apps/nestar-batch/src/lib/config.ts
@@ -5,3 +5,6 @@
 export const BATCH_ROLLBACK = 'BATCH_ROLLBACK';
 export const BATCH_TOP_PROPERTIES = 'BATCH_TOP_PROPERTIES';
 export const BATCH_TOP_AGENTS = 'BATCH_TOP_AGENTS';
+
+//next js ni routing ni hosil qilishda app router yoki pages routerdan foydalanish mumkin,
+//app router server bilan to'g'ridan to'g'ri ishlash imkonini beradi
